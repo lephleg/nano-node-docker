62075bf41db786241e98a0738d612300d2160a63
lephleg <lephleg@gmail.com>
2019-07-27 18:28:49 +0300

    Fix log directory volume for watchdog.

--- nano-node-watchdog/nano-node-watchdog.py
+++ nano-node-watchdog/nano-node-watchdog.py
@@ -16,7 +16,7 @@ from datetime import datetime
 node_ip = 'nano-node'  # node's local IP
 node_port = '7076'  # RPC port
 log_file = "/opt/nanoNodeWatchdog/log/watchdog.log"  # log file for this script
-node_log_dir = "/root/Nano/log"  # the log directory of the Nano node
+node_log_dir = "/opt/nanoNodeLog"  # the log directory of the Nano node
 # -----------------------------------------------------------------------
 
 # test whether RPC is responsive
